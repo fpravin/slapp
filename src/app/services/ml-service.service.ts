10ef4f9eaa17d5421d9136e49d37b2562252215f
Pravin Fernando <pravinfernando3@gmail.com>
2019-05-30 02:20:49 +0530

    ml done

--- src/app/services/ml-service.service.ts
+++ src/app/services/ml-service.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from "@angular/core";
 import { Storage } from "@ionic/storage";
 
+import * as brain from "brain.js";
+import { Place } from "../interfaces";
+
+
 @Injectable({
   providedIn: "root"
 })
@@ -59,11 +63,94 @@ export class MlServiceService {
 
     });
 
-
-    // console.log(Math.abs(diff));
   }
 
-  
+  async runMl(): Promise<Place[]> {
+
+    const net = new brain.NeuralNetwork();
+
+    net.train(
+      [
+        { input: [30, true, true], output: [1] },
+        { input: [30, false, false], output: [0.3] },
+        { input: [30, true, false], output: [0.8] },
+        { input: [30, false, true], output: [0.8] },
+
+        { input: [20, true, true], output: [0.8] },
+        { input: [20, false, false], output: [0.2] },
+        { input: [20, true, false], output: [0.6] },
+        { input: [20, false, true], output: [0.6] },
+
+        { input: [10, true, true], output: [0.6] },
+        { input: [10, false, false], output: [0] },
+        { input: [10, true, false], output: [0.3] },
+        { input: [10, false, true], output: [0.3] },
+      ]
+    );
+
+    let timeData: TimeMl[];
+    let countData: CountML[];
+    let favData: Favourite[];
+    let total: number = 0;
+    let avg: number = 0;
+
+    const recommendationArray: Place[] = [];
+
+    this.storage.get("favourite").then(fav => { favData = fav; });
+
+    this.storage.get("timeCount").then(time => { timeData = time; });
+
+    this.storage.get("visitCount").then(visit => { countData = visit; });
+
+    await this.storage.get("place").then(p => {
+
+      const place = p as Place[];
+
+      place.forEach(p => {
+
+        const findTime = timeData.find(t => {
+          // never gets undefined
+          return t.placeId === +p.id;
+        });
+
+        const findCount = countData.find(c => {
+          // never gets undefined
+          return c.placeId === +p.id;
+        });
+
+        const findFav = favData.find(f => {
+          // might get undefined
+          return f.place_id === +p.id;
+        });
+
+        const input: any[] = [];
+
+        input[0] = findTime.time;
+        input[1] = (findCount.count >= 3) ? true : false;
+        input[2] = (findFav) ? true : false;
+
+        for (let index = 0; index < 3; index++) {
+          const output = net.run(input);
+          total = total + output[0];
+        }
+        avg = total / 3;
+
+        console.log(avg);
+        if (avg > 0.75) {
+          recommendationArray.push(p);
+        }
+
+        total = 0;
+        avg = 0;
+
+
+
+      });
+
+    });
+
+    return recommendationArray;
+  }
 
 }
 
@@ -75,4 +162,13 @@ interface CountML {
 interface TimeMl {
   time: number;
   placeId: number;
+}
+
+interface Favourite {
+  id: number;
+  user_id: number;
+  place_id: number;
+  deleted_at: string;
+  created_at: string;
+  updated_at: string;
 }
